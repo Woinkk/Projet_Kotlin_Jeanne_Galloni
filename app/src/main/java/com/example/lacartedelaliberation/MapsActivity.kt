e4bc89da33d15d22701b74a37f58fe9d74010f49
Woinkk <Darkrai99@live.fr>
2021-10-29 09:53:51 +0200

    new project

---
+++ app/src/main/java/com/example/lacartedelaliberation/MapsActivity.kt
@@ -0,0 +1,162 @@
+package com.example.lacartedelaliberation
+
+import android.Manifest
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.lacartedelaliberation.databinding.ActivityMapsBinding
+import androidx.core.app.ActivityCompat
+
+import android.content.pm.PackageManager
+import android.location.LocationManager
+//import android.location.LocationRequest
+import android.os.Build
+import android.util.Log
+import android.util.Log.DEBUG
+import androidx.annotation.RequiresApi
+
+import androidx.core.content.ContextCompat
+import com.example.lacartedelaliberation.BuildConfig.DEBUG
+import com.google.android.gms.location.*
+import java.lang.Exception
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    // declare a global variable FusedLocationProviderClient
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+
+    // globally declare LocationRequest
+    private lateinit var locationRequest: LocationRequest
+
+    // globally declare LocationCallback
+    private lateinit var locationCallback: LocationCallback
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        try {
+            if (ContextCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    101
+                )
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        // in onCreate() initialize FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
+        val locationRequest: LocationRequest = LocationRequest.create().apply {
+            locationRequest = LocationRequest()
+            interval = 100
+            fastestInterval = 50
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            maxWaitTime= 100
+            locationCallback = object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult?) {
+                    locationResult ?: return
+
+                    if (locationResult.locations.isNotEmpty()) {
+                        // get latest location
+                        val location =
+                            locationResult.lastLocation
+                        // use your location object
+                        // get latitude , longitude and other info from this
+                        Log.d("STATE", location.toString())
+                    }
+                }
+
+            }
+        }
+        Log.d("STATE", locationManager?.getProviders(true).toString())
+
+        startLocationUpdates()
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    fun onLocationResult(locationResult: LocationResult?) {
+        locationResult ?: return
+
+        if (locationResult.locations.isNotEmpty()) {
+            // get latest location
+            val location =
+                locationResult.lastLocation
+            // use your location object
+            // get latitude , longitude and other info from this
+            Log.d("STATE", location.toString())
+        }
+
+
+    }
+
+    //start location updates
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null /* Looper */
+        )
+    }
+}
